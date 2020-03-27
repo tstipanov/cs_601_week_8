70cb2ced346555cfe87ed7b80356d467bb40bb40
Tom Stipanov <31575763+tstipanov@users.noreply.github.com>
2020-03-27 15:50:02 -0400

    Update news_data.js

--- news_data.js
+++ news_data.js
@@ -10,7 +10,7 @@ const articles = [
 	},
     	{
 		caption: "Georgia\'s hardest-hit hospital says its intensive care units are filled with \'critically ill\' coronavirus patients",
-		keywords: ["stimulus", "unemployment", "congress", "coronavirus"],
+		keywords: ["critical", "ill", "georgia", "hospital", "coronavirus"],
 		source: "CNN",
 		url: "http://www.cnn.com",
 		db_author: "Amy Bloggs",
@@ -19,7 +19,7 @@ const articles = [
 	},
     	{
 		caption: "Ethiopia pardons more than 4,000 prisoners to help prevent coronavirus spread",
-		keywords: ["stimulus", "unemployment", "congress", "coronavirus"],
+		keywords: ["ethiopia", "pardon", "prisoners", "president"],
 		source: "Fox News",
 		url: "http://www.foxnews.com",
 		db_author: "Joe Mann",
@@ -28,7 +28,7 @@ const articles = [
 	},	
     	{
 		caption: "He tweeted about his symptoms and coronavirus diagnosis problems. His husband says he was then put on a ventilator",
-		keywords: ["stimulus", "unemployment", "congress", "coronavirus"],
+		keywords: ["symptoms", "diagnosis", "coronavirus", "ventillator"],
 		source: "CNN",
 		url: "http://www.foxnews.com",
 		db_author: "Joe Mann",
@@ -37,7 +37,7 @@ const articles = [
 	},
     	{
 		caption: "Brazil's Bolsonaro calls preventive coronavirus measures \'hysteria\'",
-		keywords: ["stimulus", "unemployment", "congress", "coronavirus"],
+		keywords: ["brazil", "hysteria", "Bolsonaro", "coronavirus"],
 		source: "Fox News",
 		url: "http://www.foxnews.com",
 		db_author: "Amy Bloggs",
@@ -46,7 +46,7 @@ const articles = [
 	},
     	{
 		caption: "Warnings over harm to Israel's \'democratic system\' amid coronavirus crisis",
-		keywords: ["stimulus", "unemployment", "congress", "coronavirus"],
+		keywords: ["Israel", "democratic", "crisis", "coronavirus"],
 		source: "Newsweek",
 		url: "http://www.foxnews.com",
 		db_author: "Amy Bloggs",
@@ -55,11 +55,74 @@ const articles = [
 	},	
 	{
 		caption: "Protesters gathered outside the Knesset Thursday",
-		keywords: ["stimulus", "unemployment", "congress", "coronavirus"],
+		keywords: ["Protesters", "Knesset", "Tel Aviv", "Thursday"],
 		source: "The Voxx",
 		url: "https://www.washingtonpost.com/",
 		db_author: "Amy Bloggs",
 		released: "2019-03-24 EST",
+		story: "Hundreds of protesters, waving flags and banners from their cars, drove from Tel Aviv to Jerusalem Thursday to rally against Edelstein\'s move. Police attempted to stop the convoy as it drove along the highway, claiming they were driving slowly and causing violations. After a temporary delay, protesters succeeded in making it past the police roadblock to eventually gather outside the High Court and the Knesset. Along with Israeli flags, they waved black flags to symbolize the death of democracy. One person held a banner bearing the words in English, \"Crime Minister.\" Critics have accused Edelstein of acting in defiance of the public will. They point to the timing of a tweet he made on Sunday, just moments after President Rivlin tasked Gantz with trying to form a new government...."
+    	},
+	{
+		caption: "Unemployment spikes and Congress moves ahead on $2 trillion in aid (RERUN FOR TESTING A FUTURE DATE)",
+		keywords: ["stimulus"],
+		source: "Fox News",
+		url: "http://www.foxnews.com",
+		db_author: "Associated Press",
+		released: "2020-03-28 EST",
+		story: "Washington (CNN) The Senate's historic $2 trillion stimulus package is set for debate on the House floor Friday. The chamber will convene at 9 a.m. to consider the largest emergency aid package in US history in response to the coronavirus outbreak and its economic fallout, following a remarkable 96-0 Senate vote late Wednesday. House Speaker Nancy Pelosi -- who turns 80 today -- has said she would prefer that the Senate measure is adopted by unanimous consent, which would spare members who are isolating or at risk from having to travel back to Washington. But Democratic Rep. Alexandria Ocasio-Cortez, the New York freshman, is leaving open the option of objecting, potentially forcing House members to return and delaying a vote. Pelosi, however, told reporters that won't happen. \"We will be passing the bill tomorrow,\" she said today, CNN\'s Haley Byrd and Clare Foran report. $250 billion set aside for direct payments to individuals and families, $350 billion in small business loans and $250 billion in unemployment insurance benefits. A major amount of funding for hard-hit hospitals -- $130 billion -- as well as $150 billion for state and local governments that are cash-strapped due to their response to coronavirus. A $500 billion fund for Treasury to provide loans and support for distressed industries. That includes $25 billion for passenger air carriers, $4 billion for cargo air carriers and $17 billion for businesses that work in national security. The rest of the funds, $454 billion, can be spent as loans to businesses, states and municipalities."
+	},
+    	{
+		caption: "Georgia\'s hardest-hit hospital says its intensive care units are filled with \'critically ill\' coronavirus patients (RERUN FOR TESTING A FUTURE DATE)",
+		keywords: ["critical"],
+		source: "CNN",
+		url: "http://www.cnn.com",
+		db_author: "Associated Press",
+		released: "2020-03-29 EST",
+		story: "A southwest Georgia hospital that's reporting about a quarter of the state's coronavirus deaths says it has reached capacity in three intensive-care units. The ICUs within Phoebe Putney Memorial Hospital in Albany are filled with \"critically ill Covid-19 patients,\" Phoebe Putney Health System said in a news release Wednesday. A fourth ICU was previously opened to care for patients not infected with the virus. The system has four medical centers in Albany, Americus and Sylvester, as well as other facilities, but Phoebe Putney Memorial Hospital is facing the worst of the pandemic. Of Georgia's 47 deaths from coronavirus, 12 patients had died at the Albany facility as of Wednesday afternoon, according to the hospital system. \"As this public health crisis in southwest Georgia gets more severe, we have been reaching out to other hospitals in our part of the state,\" the system's CEO, Scott Steiner, said. \"I am pleased that every one of our regional partners we spoke to in the last 24 hours agreed to assist by accepting patient transfers from us.\""
+	},
+    	{
+		caption: "Ethiopia pardons more than 4,000 prisoners to help prevent coronavirus spread (RERUN FOR TESTING A FUTURE DATE)",
+		keywords: ["ethiopia"],
+		source: "Fox News",
+		url: "http://www.foxnews.com",
+		db_author: "Associated Press",
+		released: "2020-03-30 EST",
+		story: "Ethiopian President Sahle-Work Zewde has granted pardon to more than 4,000 prisoners in an effort to contain the spread of coronavirus. Sahle-Work Zewde announced the order in a tweet on Wednesday and said it would help prevent overcrowding in prisons. The directive only covers those given a maximum sentence of three years for minor crimes and those who were about to be released from jail, she said. There are 12 confirmed cases of Covid-19 in Ethiopia, the World Health Organization said Wednesday. Authorities in the nation have put in place a raft of measures, including the closure of all borders except to those bringing in essential goods to contain the virus. The government has directed security officials to monitor and enforce a ban on large gatherings and overcrowded public transport to ensure social distancing."
+	},	
+    	{
+		caption: "He tweeted about his symptoms and coronavirus diagnosis problems. His husband says he was then put on a ventilator (RERUN FOR TESTING A FUTURE DATE)",
+		keywords: ["symptoms"],
+		source: "CNN",
+		url: "http://www.foxnews.com",
+		db_author: "Associated Press",
+		released: "2020-03-31 EST",
+		story: "Just four days ago, David Lat was tweeting about his coronavirus symptoms and the difficulty he had getting diagnosed. The lawyer and founder of the blog \"Above the Law\" was diagnosed with coronavirus after two visits to the emergency room, according to his tweets. By Saturday evening, he was on a ventilator and in a \"very serious\" condition, his husband, Zachary Baron Shemtob, told CNN on Monday. It\'s unclear what Lat\'s current official medical condition is. \"He's still on the ventilator, he's still mostly sedated,\" though he occasionally wakes up and is able to write down questions, Shemtob said. \"One of the hardest parts here is we can't visit him. It's really tough.\""
+	},
+    	{
+		caption: "Brazil's Bolsonaro calls preventive coronavirus measures \'hysteria\' (RERUN FOR TESTING A FUTURE DATE)",
+		keywords: ["brazil"],
+		source: "Fox News",
+		url: "http://www.foxnews.com",
+		db_author: "Associated Press",
+		released: "2020-04-01 EST",
+		story: "Brazil confirmed its first coronavirus-related death on Tuesday, as President Jair Bolsonaro continued to deride efforts to mitigate the outbreak as \"hysteria.\" A 62-year-old man, who tested positive for the virus, died on Monday four days after being admitted to a hospital in Sao Paulo, officials said. The man, who suffered from hypertension and diabetes, caught the virus domestically, they said, suggesting community transmission of the virus in the South American nation. After announcing its first case on February 26, infection numbers have risen to 291. The figure is expected to multiply in the coming days, but Brazilians continue to get mixed messages from the top. In an interview on Sunday with CNN affiliate CNN Brasil, Bolsonaro called preventive measures, such as banning mass events, \"hysteria,\" and said that the economy came first."
+	},
+    	{
+		caption: "Warnings over harm to Israel's \'democratic system\' amid coronavirus crisis (RERUN FOR TESTING A FUTURE DATE)",
+		keywords: ["Israel"],
+		source: "Newsweek",
+		url: "http://www.foxnews.com",
+		db_author: "Associated Press",
+		released: "2020-04-02 EST",
+		story: "Israel\'s President has warned of possible harm to the country's democracy, after the Speaker of Parliament refused to hold key parliamentary votes. Speaker Yuli Edelstein, who has the power to decide the Knesset\'s schedule, ended Wednesday\'s session just a few minutes after it began, making it impossible to form committees to oversee government actions during the coronavirus crisis. Edelstein has also refused to hold a vote to choose a new Speaker of the Knesset, a position he has held since 2013, despite the clear parliamentary majority to replace him..."
+	},	
+	{
+		caption: "Protesters gathered outside the Knesset Thursday (RERUN FOR TESTING A FUTURE DATE)",
+		keywords: ["Protesters"],
+		source: "The Voxx",
+		url: "https://www.washingtonpost.com/",
+		db_author: "Associated Press",
+		released: "2019-04-03 EST",
 		story: "Hundreds of protesters, waving flags and banners from their cars, drove from Tel Aviv to Jerusalem Thursday to rally against Edelstein\'s move. Police attempted to stop the convoy as it drove along the highway, claiming they were driving slowly and causing violations. After a temporary delay, protesters succeeded in making it past the police roadblock to eventually gather outside the High Court and the Knesset. Along with Israeli flags, they waved black flags to symbolize the death of democracy. One person held a banner bearing the words in English, \"Crime Minister.\" Critics have accused Edelstein of acting in defiance of the public will. They point to the timing of a tweet he made on Sunday, just moments after President Rivlin tasked Gantz with trying to form a new government...."
     	}
 ];
