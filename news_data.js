31d1bb25ee442b14dacc337e4dd764988230f473
Tom Stipanov <31575763+tstipanov@users.noreply.github.com>
2020-03-27 08:35:20 -0400

    Update news_data.js

--- news_data.js
+++ news_data.js
@@ -11,7 +11,7 @@ const articles = [
 		caption: "Georgia\'s hardest-hit hospital says its intensive care units are filled with \'critically ill\' coronavirus patients",
 		source: "CNN",
 		url: "http://www.cnn.com",
-		db_author: "Joe Bloggs",
+		db_author: "Amy Bloggs",
 		released: "2020-03-26",
 		story: "A southwest Georgia hospital that's reporting about a quarter of the state's coronavirus deaths says it has reached capacity in three intensive-care units. The ICUs within Phoebe Putney Memorial Hospital in Albany are filled with \"critically ill Covid-19 patients,\" Phoebe Putney Health System said in a news release Wednesday. A fourth ICU was previously opened to care for patients not infected with the virus. The system has four medical centers in Albany, Americus and Sylvester, as well as other facilities, but Phoebe Putney Memorial Hospital is facing the worst of the pandemic. Of Georgia's 47 deaths from coronavirus, 12 patients had died at the Albany facility as of Wednesday afternoon, according to the hospital system. \"As this public health crisis in southwest Georgia gets more severe, we have been reaching out to other hospitals in our part of the state,\" the system's CEO, Scott Steiner, said. \"I am pleased that every one of our regional partners we spoke to in the last 24 hours agreed to assist by accepting patient transfers from us.\""
 	},
@@ -24,36 +24,36 @@ const articles = [
 		story: "Ethiopian President Sahle-Work Zewde has granted pardon to more than 4,000 prisoners in an effort to contain the spread of coronavirus. Sahle-Work Zewde announced the order in a tweet on Wednesday and said it would help prevent overcrowding in prisons. The directive only covers those given a maximum sentence of three years for minor crimes and those who were about to be released from jail, she said. There are 12 confirmed cases of Covid-19 in Ethiopia, the World Health Organization said Wednesday. Authorities in the nation have put in place a raft of measures, including the closure of all borders except to those bringing in essential goods to contain the virus. The government has directed security officials to monitor and enforce a ban on large gatherings and overcrowded public transport to ensure social distancing."
 	},	
     	{
-		caption: "Trump adopted by the Queen of Yorkshire",
+		caption: "He tweeted about his symptoms and coronavirus diagnosis problems. His husband says he was then put on a ventilator",
 		source: "CNN",
 		url: "http://www.foxnews.com",
 		db_author: "Joe Mann",
-		released: "2020-01-17 12:01:28 AM EST",
-		story: "This is a really interesting story.  More to follow.."
+		released: "2020-03-24",
+		story: "Just four days ago, David Lat was tweeting about his coronavirus symptoms and the difficulty he had getting diagnosed. The lawyer and founder of the blog \"Above the Law\" was diagnosed with coronavirus after two visits to the emergency room, according to his tweets. By Saturday evening, he was on a ventilator and in a \"very serious\" condition, his husband, Zachary Baron Shemtob, told CNN on Monday. It\'s unclear what Lat\'s current official medical condition is. \"He's still on the ventilator, he's still mostly sedated,\" though he occasionally wakes up and is able to write down questions, Shemtob said. \"One of the hardest parts here is we can't visit him. It's really tough.\""
 	},
     	{
-		caption: "Trump adopted by the Queen of Yorkshire",
+		caption: "Brazil's Bolsonaro calls preventive coronavirus measures \'hysteria\'",
 		source: "Fox News",
 		url: "http://www.foxnews.com",
-		db_author: "Joe Mann",
-		released: "2020-01-17 12:01:28 AM EST",
-		story: "This is a really interesting story.  More to follow.."
+		db_author: "Amy Bloggs",
+		released: "2020-03-24",
+		story: "Brazil confirmed its first coronavirus-related death on Tuesday, as President Jair Bolsonaro continued to deride efforts to mitigate the outbreak as \"hysteria.\" A 62-year-old man, who tested positive for the virus, died on Monday four days after being admitted to a hospital in Sao Paulo, officials said. The man, who suffered from hypertension and diabetes, caught the virus domestically, they said, suggesting community transmission of the virus in the South American nation. After announcing its first case on February 26, infection numbers have risen to 291. The figure is expected to multiply in the coming days, but Brazilians continue to get mixed messages from the top. In an interview on Sunday with CNN affiliate CNN Brasil, Bolsonaro called preventive measures, such as banning mass events, \"hysteria,\" and said that the economy came first."
 	},
     	{
-		caption: "Trump adopted by the Queen of Yorkshire",
+		caption: "Warnings over harm to Israel's \'democratic system\' amid coronavirus crisis",
 		source: "Newsweek",
 		url: "http://www.foxnews.com",
-		db_author: "Joe Mann",
-		released: "2020-01-17 12:01:28 AM EST",
-		story: "This is a really interesting story.  More to follow.."
+		db_author: "Amy Bloggs",
+		released: "2020-03-24",
+		story: "Israel\'s President has warned of possible harm to the country's democracy, after the Speaker of Parliament refused to hold key parliamentary votes. Speaker Yuli Edelstein, who has the power to decide the Knesset\'s schedule, ended Wednesday\'s session just a few minutes after it began, making it impossible to form committees to oversee government actions during the coronavirus crisis. Edelstein has also refused to hold a vote to choose a new Speaker of the Knesset, a position he has held since 2013, despite the clear parliamentary majority to replace him..."
 	},	
 	{
-		caption: "Dog for hire.  Solves crime and fluffy",
+		caption: "Protesters gathered outside the Knesset Thursday",
 		source: "The Voxx",
 		url: "https://www.washingtonpost.com/",
-		db_author: "Mary Blige",
-		released: "2019-11-17 8:21:01 PM EST",
-		story: "Makes make hope that a dog can become President.  Hell, Trump can do it..."
+		db_author: "Amy Bloggs",
+		released: "2019-03-24",
+		story: "Hundreds of protesters, waving flags and banners from their cars, drove from Tel Aviv to Jerusalem Thursday to rally against Edelstein\'s move. Police attempted to stop the convoy as it drove along the highway, claiming they were driving slowly and causing violations. After a temporary delay, protesters succeeded in making it past the police roadblock to eventually gather outside the High Court and the Knesset. Along with Israeli flags, they waved black flags to symbolize the death of democracy. One person held a banner bearing the words in English, \"Crime Minister.\" Critics have accused Edelstein of acting in defiance of the public will. They point to the timing of a tweet he made on Sunday, just moments after President Rivlin tasked Gantz with trying to form a new government...."
     	}
 ];
 
